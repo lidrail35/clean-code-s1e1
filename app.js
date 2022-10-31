e612fe30f98bc5bb5868157e0ce5ccd6c4690bdf
lidrail35 <lidrail35@mail.ru>
2022-10-31 23:54:35 +0300

    refactor: change input[type=text] tag by class input-text according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task input-text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit button";
@@ -83,7 +83,8 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    //var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector(".input-text");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("editMode");

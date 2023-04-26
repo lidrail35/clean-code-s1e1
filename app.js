fd0be7cd69e6cf166eaf4dbae67e12a51911b408
lidrail35 <lidrail35@mail.ru>
2023-04-26 23:10:38 +0300

    refactor: change name class according to rule 3.1 in html-and-css.md

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 var taskInput=document.querySelector(".new");//Add a new task.
 //var addButton=document.getElementsByTagName("button")[0];//first button
 var addButton=document.querySelectorAll("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -91,7 +91,7 @@ var editTask=function(){
     var editInput=listItem.querySelector(".input-text");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
+    var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -105,7 +105,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("edit-mode");
 };
 
 
